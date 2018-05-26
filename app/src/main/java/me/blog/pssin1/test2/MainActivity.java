ab8a310347897304c6fb86d796d41c2e605b2ea9
GunHo <pssin1@naver.com>
2018-05-26 12:27:14 +0900

    Initial commit

---
+++ app/src/main/java/me/blog/pssin1/test2/MainActivity.java
@@ -0,0 +1,55 @@
+package me.blog.pssin1.test2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addClick(View v)
+    {
+        EditText number1 = findViewById(R.id.number1);
+        EditText number2 = findViewById(R.id.number2);
+        TextView result = findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1 + n2));
+    }
+
+    public void subClick(View v)
+    {
+        EditText number1 = findViewById(R.id.number1);
+        EditText number2 = findViewById(R.id.number2);
+        TextView result = findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1 - n2));
+    }
+
+    public void multiClick(View v)
+    {
+        EditText number1 = findViewById(R.id.number1);
+        EditText number2 = findViewById(R.id.number2);
+        TextView result = findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1 * n2));
+    }
+
+    public void divideClick(View v)
+    {
+        EditText number1 = findViewById(R.id.number1);
+        EditText number2 = findViewById(R.id.number2);
+        TextView result = findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1 / n2));
+    }
+}
